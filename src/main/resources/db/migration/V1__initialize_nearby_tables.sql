c0019aa227f8560d0bdb5f92c4bdd3bf479fd6b3
Obaid Hasseeb <hasseeb.obaid.2016@gmail.com>
2022-07-13 18:10:40 -0700

    Updated V1_initialize_nearby_tables.sql to create tables automatically

--- src/main/resources/db/migration/V1__initialize_nearby_tables.sql
+++ src/main/resources/db/migration/V1__initialize_nearby_tables.sql
@@ -1,65 +1,76 @@
--- CREATE TABLE IF NOT EXISTS user_account (
---     id SERIAL PRIMARY KEY,
---     alt_id VARCHAR(32),
---     created timestamp
--- );
-
-
-CREATE TABLE users (
-
-id SERIAL [pk,increment],
+CREATE table if not EXISTS users (
+id SERIAL PRIMARY KEY,
 first_name VARCHAR(50),
 last_name VARCHAR(50),
 email VARCHAR(100),
 password VARCHAR(50),
 avatar VARCHAR(200),
-created_at  TIMESTAMP	
+created_at  TIMESTAMP
 );
 
-CREATE TABLE followers ( 
-id  SERIAL [pk, increment] ,
-product_id INTEGER [ref: > products.id],
-user_id INTEGER [ref: > users.id]
-created_at TIMESTAMP
-);
 
-CREATE TABLE products (
-Id SERIAL [pk , increment] 
+
+CREATE TABLE IF NOT EXISTS products (
+id SERIAL PRIMARY key,
 title VARCHAR(100),
 price VARCHAR(50),
 product_image VARCHAR(200),
 description  VARCHAR(10000),
 is_sold BOOLEAN,
-seller_id INTEGER [ ref: > users.id] ,
-buyer_id INTEGER [ ref: > users.id],
+seller_id INTEGER,
+FOREIGN KEY (seller_id) REFERENCES users(id),
+buyer_id INTEGER,
+FOREIGN KEY (buyer_id) REFERENCES users(id),
+
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
 
 
-CREATE TABLE conversations ( 
- Id SERIAL [ pk, increment],
- host_id INTEGER [ref: > users.id],
- participant_id INTEGER [ref: > users.id] ,
-created_at TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS followers (
+id  SERIAL PRIMARY KEY ,
+product_id INTEGER,
+user_id INTEGER,
+created_at TIMESTAMP,
+FOREIGN KEY (product_id) REFERENCES products(id),
+FOREIGN KEY (user_id) REFERENCES users(id)
 
-CREATE TABLE messages (
-id_ SERIAL [pk, increment],
-conversation_id INTEGER [ref: > conversations.id],
-author_id INTEGER [ref: > users.id],
-content VARCHAR,
-created_at TIMESTAMP
+-- user_id INTEGER [ref: > users.id]
 );
 
 
-CREATE TABLE garages(
-Id SERIAL [pk,increment]
+CREATE TABLE IF NOT EXISTS conversations (
+ id SERIAL PRIMARY KEY,
+ host_id INTEGER,
+ FOREIGN KEY (host_id) REFERENCES users(id),
+participant_id INTEGER,
+FOREIGN KEY (participant_id) REFERENCES users(id),
+
+created_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS garages(
+id SERIAL PRIMARY key,
 title VARCHAR(100),
 product_image VARCHAR(200),
 description VARCHAR(10000),
 updated_at TIMESTAMP,
 created_at TIMESTAMP,
-seller_id INTEGER [ref: > users.id]
-
+seller_id INTEGER,
+--  [ref: > users.id]
+FOREIGN KEY (seller_id) REFERENCES users(id)
 );
+
+
+CREATE TABLE IF NOT EXISTS messages (
+id SERIAL PRIMARY KEY,
+conversation_id INTEGER,
+--  [ref: > conversations.id],
+FOREIGN KEY (conversation_id) REFERENCES conversations(id),
+author_id INTEGER,
+-- [ref: > users.id],
+FOREIGN KEY (author_id) REFERENCES users(id),
+content VARCHAR,
+created_at TIMESTAMP
+);
+
